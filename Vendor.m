5ed3d54d5ec6027c9b926fe667263f6773d02579
JarodMcK <49254859+JarodMcK@users.noreply.github.com>
2019-04-12 19:26:21 -0400

    Update Vendor.m

--- Vendor.m
+++ Vendor.m
@@ -5,6 +5,7 @@ classdef Vendor < handle
         delivery_time = 0 %variable to hold the due date for this part
         part_delivered = 0 %true (1) or false (2) variable to say whether the part was delivered
         waiting_to_send = 0 %variable that tells the timer to keep checking till the part is sent
+        early_delivery = 0  %variable from 0 to 1 that marks the percent of the delivery time that the Job shop wants the part delivered
     end
  %Read Work Orders and determine if they need to send a part
  
@@ -29,13 +30,13 @@ methods
   
   %%%%%I need the global time to check against the delivery time here as the variable "the_time"%%%%
   %Check to see if they can send the part yet
-      If (the_time = delivery_time)
+      If (the_time == delivery_time*(1-early_delivery))
           
           %flag the part as delivered
           part_deliverd = 1;
           %reset the waiting to send flag
           waiting_to_send = 0;
-          %%%%%% Question: can I just change the work order variable like this so that the work order doesn't want anymore?
+          %%%%%% This next line can vary depending on how the part is requested
           obj.vendor_part_needed = 0;
         
         end
