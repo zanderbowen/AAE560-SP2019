e7136c312d1046c3c0b966b84f30321f63c9c1f0
dboneshc <49176212+dboneshc@users.noreply.github.com>
2019-04-18 14:08:46 -0400

    Update PoC_ProofConcept.m

--- PoC_ProofConcept.m
+++ PoC_ProofConcept.m
@@ -46,6 +46,33 @@ js_sch=JobShopSchedule;
 
 %add WOs to master schedule
 [js_sch.master_schedule revised_wo_dates]=addWoToMasterSchedule(js_sch,js_wos(masterSchedule(dir, js_wos)));
+
+%cludge to set js_wos.master_schedule to 1
+temp=findobj(js_wos,'master_schedule',0);
+for i=1:length(temp)
+    js_wos(temp(i).unique_id).master_schedule=1;
+end
+
+%setting properties in customer to tell WO to generate more WOs
+cust.new_wo=1;
+cust.new_wo_due_date=20;
+
+%make this an array of WorkOrder objects
+js_wos=genWO(js_wos,cust.new_wo,cust.new_wo_due_date,[js_wos.unique_id]);
+
+for i=1:length(js_wos)
+    if strcmp(js_wos(i).status,'new')
+        [js_wos(i).routing,js_wos(i).status]=generateRouting(dir);
+    end
+    
+    if strcmp(js_wos(i).status,'planned')
+        [js_wos(i).start_date js_wos(i).cp_duration]=calculateStartDate(js_wos(i));
+    end
+end
+
+%add new WO to master schedule
+[js_sch.master_schedule revised_wo_dates]=addWoToMasterSchedule(js_sch,js_wos(masterSchedule(dir, js_wos)));
+
 %plotting the graph of the network schedule
 figure;
 h=plot(js_sch.master_schedule,'EdgeLabel',js_sch.master_schedule.Edges.EdgeLabel);
