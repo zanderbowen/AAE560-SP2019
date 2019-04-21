dd7a4418fac128f16dd526ad9e19dda5b04330b9
dboneshc <49176212+dboneshc@users.noreply.github.com>
2019-04-21 18:51:59 -0400

    Update Vendor.m

--- Vendor.m
+++ Vendor.m
@@ -5,6 +5,7 @@ classdef Vendor < handle
         unique_id % unique identifier for each instantiated vendor object
         vendor_po % structure that contains information vendor requires to deliver a part
         early_delivery = 0  %integer of time units that the job shop desires an early delivery
+        actual_delivery_date %deterministic: obj.vendor_po.delivery_date=actual_delivery_date stochastic: based on a PDF
     end
  %Read Work Orders and determine if they need to send a part
  
@@ -40,9 +41,10 @@ classdef Vendor < handle
                         %assuming only a single vendor in this model
                         %obj.vendor_po=struct('wo_id',[],'operation',{},'delivery_date',[],'status',{});
                         obj(1).vendor_po.wo_id(ct)=u_id; %WO unique ID
-                        obj(1).vendor_po.operation{ct}=r_table.Operation(row_index); %WO Operatoin
+                        obj(1).vendor_po.operation{ct}=r_table.Operation(row_index(j)); %WO Operatoin
                         %find the row index in the master schedule containing the early start for the operationa that requires a vendor part
-                        ms_edge_name={[num2str(u_id),'.',char(r_table.Operation(row_index))]};
+                        %!!! assumes there is only one operation with that specific name in the master schedule !!!
+                        ms_edge_name={[num2str(u_id),'.',char(r_table.Operation(row_index(j)))]};
                         ms_row_index=find(contains(ms_table.EdgeLabel,ms_edge_name));
                         obj(1).vendor_po.delivery_date(ct)=ms_table.ES(ms_row_index)-obj(1).early_delivery; %Delivery date
                         %make sure the delivery date is not less than zero
@@ -52,9 +54,31 @@ classdef Vendor < handle
                         
                         %mark the PO status to 'received'
                         obj(1).vendor_po.status{ct}='received';
+                        ct=ct+1;
                     end
                 end
-                ct=ct+1;
+            end
+        end
+        
+        %method that delivers the parts to the JS, by setting the
+        %PartDelivered flag in obj.routing.Edges.PartDelivered to 1
+        function [obj js_wos]=deliverPart(obj,js_wos,current_time)
+            %deterministic: obj.vendor_po.delivery_date=actual_delivery_date stochastic: based on a PDF
+            obj(1).actual_delivery_date=obj(1).vendor_po.delivery_date;
+            %find all the parts that have a received PO status
+            v_po_index=find(strcmp(obj(1).vendor_po.status,'received'));
+            
+            for i=1:length(v_po_index)
+                %check to see if they can be delivered
+                if obj(1).actual_delivery_date(v_po_index(i))==current_time
+                    %set PartDelivered flag in WO to 1
+                    %find row index in WO routing table
+                    wo_id=obj(1).vendor_po.wo_id(v_po_index(i));
+                    r_table=js_wos(wo_id).routing.Edges;
+                    r_table_index=find(strcmp(r_table.Operation,obj(1).vendor_po.operation{v_po_index(i)}));
+                    js_wos(wo_id).routing.Edges.PartDelivered(r_table_index)=1;
+                    obj(1).vendor_po.status{v_po_index(i)}='delivered';
+                end
             end
         end
     end
