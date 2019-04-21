42dd29926c94c135c43fb055184b6e0c3f2b971f
dboneshc <49176212+dboneshc@users.noreply.github.com>
2019-04-21 01:10:53 -0400

    Update Vendor.m

--- Vendor.m
+++ Vendor.m
@@ -11,37 +11,15 @@ classdef Vendor < handle
  %Read Work Orders and determine if they need to send a part
  
 methods
- function checkWorkOrder
- 
-    %Vendor if the part is flagged as needed in Work Order then set delivery time variable to the due date.
-    If (obj.vendor_part_needed == 1)
-  
-         delivery_time = obj.delivery_time;
-         part_delivered = 0;
-         waiting_to_send = 1;
-     end
- 
- %Read the schedule to see when to deliver the part
- %Notify the Receiving object that the part has been sent
- 
- function sendVendorPart
-  
-  %Check to see if there is a part in the queue
-  If (waiting_to_send == 1)
-  
-  %%%%%I need the global time to check against the delivery time here as the variable "the_time"%%%%
-  %Check to see if they can send the part yet
-      If (the_time == delivery_time*(1-early_delivery))
-          
-          %flag the part as delivered
-          part_deliverd = 1;
-          %reset the waiting to send flag
-          waiting_to_send = 0;
-          %%%%%% This next line can vary depending on how the part is requested
-          obj.vendor_part_needed = 0;
-        
+    
+    %Constructor Method
+    function obj = Vendor(unique_id,id_vec) %Creates supervisor object
+        %check to make sure that a supervisor is not already assigned to the functional group
+        if isempty(find(id_vec==unique_id))
+            obj.unique_id=unique_id;
+        else
+            error(['This identifier is already used by Vendor ',num2str(unique_id),'.']);
         end
-     end
-  end
- end
+    end
+end
 end
