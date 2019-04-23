9c8372d1457f2e175d29a4929c8550a757a3f2bc
mchrisler <49256676+mchrisler@users.noreply.github.com>
2019-04-22 19:28:41 -0500

    Update createJobShop.m

--- createJobShop.m
+++ createJobShop.m
@@ -7,12 +7,13 @@ function [ dir, sup, mach, rec ] = createJobShop( no_dir, no_sup, no_mach, no_re
 %   This function allows a user to create a single job shop configuration.
 %   The user provides the number of directors, supervisors, machines, and
 %   receiving focals, and the function instantiates the corresponding
-%   object arrays.  
+%   object arrays.  After initializing an array, the createJobShopConfig
+%   function is called to layout the configuration.
 %
 %   Inputs:
 %   no_dir      Number of Directors
 %   no_sup      Number of Supervisors
-%   no_mach     Number of Machines
+%   no_mach     Number of Machines (MUST BE DIVISIBLE BY no_sup!)
 %   no_rec      Number or Receiving Focals
 %--------------------------------------------------------------------------
 
@@ -21,24 +22,44 @@ function [ dir, sup, mach, rec ] = createJobShop( no_dir, no_sup, no_mach, no_re
     %------------------------------------------
     
     %Director Object(s):
-    for i = 1:no_dir
-        dir(i)=Director();
+    if no_dir > 0
+        for i = 1:no_dir
+            dir(i)=Director();
+        end
+    else
+        disp('No directors generated')
     end
     
     %Supervisor Object(s):
-    for i = 1:no_sup
-        sup(i)=Supervisor();
+    if no_sup > 5
+        disp('Number of supervisors exceeds maximum allowed (5)')
+        else if no_sup > 0
+            sup = Supervisor.empty;
+        else
+            disp('No supervisors generated')
+        end
     end
     
     %Machine Object(s):
-    for i = 1:no_mach
-        mach(i)=Machine();
+    if no_mach > 10
+        disp('Number of machines exceeds maximum allowed (5)')
+        else if no_mach > 0
+            mach = Machine.empty;
+        else
+            disp('No machines generated')
+        end
     end
     
     %Receiving Object(s):
-    for i = 1:no_rec
-        rec(i)=Receiving();
+    if no_rec > 0
+        for i = 1:no_rec
+            rec(i)=Receiving();
+        end
+    else
+        disp('No receiving focals generated')
     end
-
+    
+    %Create the Job Shop Configuration
+    [sup, mach] = createJobShopConfig(no_sup, no_mach, sup, mach);
+    
 end
-
