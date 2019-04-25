c27f38013cbdfcaf02f48d06a4620f5fc2db626c
dboneshc <49176212+dboneshc@users.noreply.github.com>
2019-04-24 22:37:42 -0400

    Update NetworkMeasures.m

--- NetworkMeasures.m
+++ NetworkMeasures.m
@@ -38,13 +38,17 @@ node_table=G.Nodes;
 h=plot(G);
 
 %find all the customers
-cust_index=find(contains(G.Nodes.Name,'Customer'))
+cust_index=find(contains(G.Nodes.Name,'Customer'));
+ven_index=find(contains(G.Nodes.Name,'Vendor'));
 
-%source_index=[];
-%sink_index=[];
+mach_index=find(contains(G.Nodes.Name,'Machine'));
+ERP_index=find(contains(G.Nodes.Name,'ERP'));
+
+source_indicies=[cust_index,ven_index];
+sink_indicies=[mach_index,ERP_index];
 
 %layout(h,'force3','WeightEffect','direct');
 %layout(h,'layered','AssignLayers','alap');
-layout(h,'layered','Direction','down','Sources',{'Customer.1','Vendor.1'},'Sinks',{'Machine.A1','ERP'});
+layout(h,'layered','Direction','down','Sources',G.Nodes.Name(source_indicies),'Sinks',G.Nodes.Name(sink_indicies));
 
 end
