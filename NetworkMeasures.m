adfb9d2d33f8dbe14033cd9e6424fc94d3a17dfc
dboneshc <49176212+dboneshc@users.noreply.github.com>
2019-04-24 21:33:06 -0400

    Update NetworkMeasures.m

---
+++ NetworkMeasures.m
@@ -0,0 +1,37 @@
+function node_table = ClusteringCoefficient(G)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+%number of nodes
+nnodes=numnodes(G);
+
+%pull out the adjacency matrix
+
+%calculate number of triangles
+triangles=diag(A^3)./2;
+
+for i=1:nnodes
+    if degree(G,i)<2
+        triples(i)=0;
+    else
+        triples(i)=nchoosek(degree(G,i),2);
+    end
+end
+
+%clustering coefficient
+CC=triangles'./triples;
+
+%add the clustering coefficient calculation to the graph node table
+G.Nodes.ClusteringCoeff=CC';
+
+%betweeness centrality
+B=centrality(G,'betweenness');
+B=B*(2/((nnodes-1)*(nnodes-2)));
+
+% add the betweeness centrality calculation to the graph node table
+G.Nodes.BCentrality=B;
+
+node_table=G.Nodes;
+
+end
+
