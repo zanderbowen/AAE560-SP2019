0bbe775ccae457d6f87628f7abeaeb037657284a
mchrisler <49256676+mchrisler@users.noreply.github.com>
2019-04-29 17:17:10 -0500

    Update routing.m

--- routing.m
+++ routing.m
@@ -3,6 +3,15 @@ function [js_wos] = routing( js_wos, dir )
 for i=1:length(js_wos)
     if strcmp(js_wos(i).status,'new')
         [js_wos(i).routing,js_wos(i).status]=generateRouting(dir);
+        
+        ops = length(js_wos(i).routing.Edges.VendorPart);
+        r = randi([1 2], 1, ops);
+        
+        for j = 1:ops
+            if r(j) == 1
+            js_wos(i).routing.Edges.VendorPart{j}='required';
+            end
+        end
     end
     
     if strcmp(js_wos(i).status,'planned') && js_wos(i).master_schedule==0
