f965195daf3ecae4d8a98d7c3704812cdc49ce66
dboneshc <49176212+dboneshc@users.noreply.github.com>
2019-04-30 23:45:28 -0400

    Update dataReduction.m

--- dataReduction.m
+++ dataReduction.m
@@ -18,3 +18,16 @@ std_SV=std(total_SV_vec);
 %display results
 disp(['The mean schedule variance is ',num2str(mean_SV),'.']);
 disp(['The standard distribution of schedule variance is ',num2str(std_SV),'.']);
+
+% *** figure defaults ***
+width = 6;     % Width in inches
+height = 4;    % Height in inches
+alw = 0.75;    % AxesLineWidth
+fsz = 11;      % Fontsize
+lw = 1.5;      % LineWidth
+msz = 8;       % MarkerSize
+% ************************
+% figure;
+% pos = get(gcf, 'Position');
+% set(gcf, 'Position', [pos(1) pos(2) width*100, height*100]); %<- Set size
+% set(gca, 'FontSize', fsz, 'LineWidth', alw); %<- Set properties
