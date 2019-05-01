49076507d179ad5139593c5c077ba7d7f88c2483
mchrisler <49256676+mchrisler@users.noreply.github.com>
2019-04-30 19:14:39 -0500

    Update scheduleVariance.m

--- scheduleVariance.m
+++ scheduleVariance.m
@@ -10,11 +10,52 @@ target = comm_net.Edges.EndNodes(:,2);
 weights = comm_net.Edges.Weight;
 G = graph(source, target, weights);
 
-[dir_P, d_length] = shortestpath(G,'Director','Machine.A1');
-[ven_P, v_length] = shortestpath(G,'Vendor.1','Machine.A1');
+if mach_no == 1
 
-dir_time_reduction = 1/d_length;
-ven_time_reduction = 1/v_length;
+    [dir_P, d_length] = shortestpath(G,'Director','Machine.A1');
+    [ven_P, v_length] = shortestpath(G,'Vendor.1','Machine.A1');
+
+    dir_time_reduction = 1/d_length;
+    ven_time_reduction = 1/v_length;
+    
+elseif mach_no == 2
+        
+    [dir_P, d_length] = shortestpath(G,'Director','Machine.B2');
+    [ven_P, v_length] = shortestpath(G,'Vendor.1','Machine.B2');
+
+    dir_time_reduction = 1/d_length;
+    ven_time_reduction = 1/v_length;
+    
+elseif mach_no == 3
+    
+    [dir_P, d_length] = shortestpath(G,'Director','Machine.C3');
+    [ven_P, v_length] = shortestpath(G,'Vendor.1','Machine.C3');
+
+    dir_time_reduction = 1/d_length;
+    ven_time_reduction = 1/v_length;
+    
+elseif mach_no == 4
+    
+    [dir_P, d_length] = shortestpath(G,'Director','Machine.D4');
+    [ven_P, v_length] = shortestpath(G,'Vendor.1','Machine.D4');
+
+    dir_time_reduction = 1/d_length;
+    ven_time_reduction = 1/v_length;
+    
+elseif
+    
+    [dir_P, d_length] = shortestpath(G,'Director','Machine.E5');
+    [ven_P, v_length] = shortestpath(G,'Vendor.1','Machine.E5');
+
+    dir_time_reduction = 1/d_length;
+    ven_time_reduction = 1/v_length;
+    
+else
+    disp('Too many machines!');
+        
+end
+    
+
 
 if actual_duration > planned_duration
     total_time_reduction = dir_time_reduction + ven_time_reduction;
